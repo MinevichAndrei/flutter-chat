f404a299c13a47095081fb311bc138030788dc25
Andrei Minevich <mav@oac.gov.by>
2021-07-22 17:50:50 +0300

    CLOSES: Create views

---
+++ lib/views/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chats"),
+      ),
+    );
+  }
+}
