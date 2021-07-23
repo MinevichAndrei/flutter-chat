823158237837161e9f6ded9a49268d46ada7d728
Andrei Minevich <mav@oac.gov.by>
2021-07-23 15:37:57 +0300

    Create services

---
+++ lib/services/auth.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthMethods {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  getCurrentUser() {
+    return auth.currentUser;
+  }
+
+  signInWithGoogle(BuildContext context) async {
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount!.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    UserCredential result =
+        await _firebaseAuth.signInWithCredential(credential);
+    User? userDetails = result.user;
+    if (result != null) {}
+  }
+}
