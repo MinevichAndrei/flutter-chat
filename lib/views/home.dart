e830c8772741caf7fe1b7491c8a588084433311e
Andrei Minevich <mav@oac.gov.by>
2021-07-26 11:16:48 +0300

    Create chat home screen

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/services/auth.dart';
+import 'package:flutter_chat/views/sign_in.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,11 +10,84 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isSearhing = false;
+  TextEditingController searchUserNameEditingController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        actions: [
+          InkWell(
+            onTap: () {
+              AuthMethods().signOut().then((s) {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => SignIn()));
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.exit_to_app),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                isSearhing
+                    ? GestureDetector(
+                        onTap: () {
+                          isSearhing = false;
+                          searchUserNameEditingController.text = "";
+                          setState(() {});
+                        },
+                        child: Padding(
+                          child: Icon(Icons.arrow_back),
+                          padding: EdgeInsets.only(right: 12),
+                        ),
+                      )
+                    : Container(),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 16),
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextField(
+                          controller: searchUserNameEditingController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'username',
+                          ),
+                        )),
+                        GestureDetector(
+                          onTap: () {
+                            isSearhing = true;
+                            setState(() {});
+                          },
+                          child: Icon(Icons.search),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
